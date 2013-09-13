2be6a4f5abb289776381b70f256b313e6fb358f8
Marcus Burnette <marcusdburnette@gmail.com>
2013-09-12 23:36:30 -0400

    convert to plugin and create demo
    convert javascript file to jquery plugin and created a demo html page

--- jquery.smoothanchors.js
+++ jquery.smoothanchors.js
@@ -1,34 +1,41 @@
-jQuery(function($) {
-// ---------------------------------------------------------------------------------------------
+$.fn.smoothAnchors = function(options){
 
-// --------------------------------------------- set variables --
-var scrollAnchors = $('a[href^=#]'),
-	scrollSpeed = 1000,
-	topOffset = 50;
+	var defaults = {
 
-// --------------------------------------------- trigger on click event --
-scrollAnchors.click(function(e){
+		topOffset: 50,
+		scrollSpeed: 1000
 
-	var el = $(this),
-		elLink = el.attr("href");
-		
-	// ----- trigger if link isn't just # (placeholder href) -----
-	if(elLink != '#'){
-		var lmntName = elLink.replace('#',''),
-			lmnt = $('a[name='+lmntName+']'),
-			
-			// ----- find top offset of target element -----
-			scrollAmt = lmnt.offset().top,
-			scrollAmt = parseInt(scrollAmt) - topOffset;
-		
-		// ----- animate to target element (minus topOffset) -----
-		$('html,body').animate({ scrollTop: scrollAmt }, scrollSpeed);
-	}
-	
-	// ----- prevent default click action -----
-	e.preventDefault();
-});
+	};
 
+	var config = $.extend(defaults, options);
 
-// ---------------------------------------------------------------------------------------------
-});
+	this.each(function(){
+
+		$(this).click(function(e){
+			var el = $(this),
+				elLink = el.attr("href");
+
+			// ----- trigger if link isn't just # (placeholder href) -----
+			if(elLink != '#'){
+				var lmntName = elLink.replace('#',''),
+					lmnt = $('a[name='+lmntName+']');
+
+					// ----- find top offset of target element -----
+				var scrollAmt = lmnt.offset().top;
+				console.log(scrollAmt);
+				scrollAmt = parseInt(scrollAmt) - config.topOffset;
+				console.log(scrollAmt);
+
+				// ----- animate to target element (minus topOffset) -----
+				$('html,body').animate({ scrollTop: scrollAmt }, config.scrollSpeed);
+			}
+
+			// ----- prevent default click action -----
+			e.preventDefault();
+		});
+
+	});
+
+	return this;
+
+}
