a6afe2d708838d308bb261425ec54f8a944a1dbf
Marcus Burnette <marcusdburnette@gmail.com>
2016-04-24 22:36:50 -0400

    fixed to use with anchors targeting an element's ID

--- jquery.smoothanchors.js
+++ jquery.smoothanchors.js
@@ -11,26 +11,32 @@ jQuery.fn.smoothAnchors = function(options){
 
 	this.each(function(){
 
-		jQuery(this).click(function(e){
+		jQuery(this).on('click', function(e){
+			// ----- prevent default click action -----
+			e.preventDefault();
+			
 			var el = jQuery(this),
 				elLink = el.attr("href");
 
 			// ----- trigger if link isn't just # (placeholder href) -----
 			if(elLink != '#'){
 				var lmntName = elLink.replace('#',''),
+					lmnt;
+				
+				// check if targeting "name" or "id" of an element	
+				if(jQuery('a[name='+lmntName+']').length > 0){
 					lmnt = jQuery('a[name='+lmntName+']');
+				} else if(jQuery('[id='+lmntName+']').length > 0){
+					lmnt = jQuery('[id='+lmntName+']');
+				}
 
-					// ----- find top offset of target element -----
+				// ----- find top offset of target element -----
 				var scrollAmt = lmnt.offset().top;
-				console.log(scrollAmt);
 				scrollAmt = parseInt(scrollAmt) - config.topOffset;
-				console.log(scrollAmt);
 
 				// ----- animate to target element (minus topOffset) -----
 				jQuery('html,body').animate({ scrollTop: scrollAmt }, config.scrollSpeed);
 				
-				// ----- prevent default click action -----
-				e.preventDefault();
 			}
 
 		});
